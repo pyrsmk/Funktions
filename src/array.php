51e98205313f341c1bfd7dfefabbc57c3fc41151
Aurélien Delogu <dev@dreamysource.fr>
2019-08-14 19:17:13 +0200

    Add loop() and loop_until()

--- src/array.php
+++ src/array.php
@@ -368,15 +368,55 @@ function kmin(array $array)
 }
 
 /**
+ * Loop over items and pass them to a generator
+ *
+ * @param iterable $iterable
+ * @param callable $callable
+ * @return array
+ */
+function loop(iterable $iterable, callable $callable): array
+{
+    $array = [];
+    foreach ($iterable as $item) {
+        $generator = call_user_func($callable, $item);
+        if ($generator instanceof Generator === false) {
+            throw Exception('callable must be a generator');
+        }
+        $array = array_merge($array, iterator_to_array($generator));
+    }
+    return $array;
+}
+
+/**
+ * Loop in a generator until a returned condition
+ *
+ * @param callable $callable
+ * @return array
+ */
+function loop_until(callable $callable): array
+{
+    $array = [];
+    do {
+        $generator = call_user_func($callable);
+        if ($generator instanceof Generator === false) {
+            throw Exception('callable must be a generator');
+        }
+        $array = array_merge($array, iterator_to_array($generator));
+    }
+    while(!$generator->getReturn());
+    return $array;
+}
+
+/**
  * array_map() with parameters in the right order
  *
  * @param array $array
  * @param callable $callback
  * @return array
  */
-function map(array $array, callable $callback): array
+function map(array $array, callable $callable): array
 {
-    return array_map($callback, $array);
+    return array_map($callable, $array);
 }
 
 /**
